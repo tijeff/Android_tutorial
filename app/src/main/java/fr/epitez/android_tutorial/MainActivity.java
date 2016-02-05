62a47dedc7d589084a0b3c88c445803608cb6ae2
Jean-François Marronnier <jfmarronnier@epitez.fr>
2016-02-05 01:06:31 +0100

    Application appellant un Intent

--- app/src/main/java/fr/epitez/android_tutorial/MainActivity.java
+++ app/src/main/java/fr/epitez/android_tutorial/MainActivity.java
@@ -1,7 +1,10 @@
 package fr.epitez.android_tutorial;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClick(View view) {
+        String url="http://www.epitez.fr";
+        Intent i = new Intent(Intent.ACTION_VIEW);
+        i.setData(Uri.parse(url));
+        startActivity(i);
     }
 }
